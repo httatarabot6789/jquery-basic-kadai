35454f830aca01c22be9fab5291438aa8e394007
httatarabot6789 <157340029+httatarabot6789@users.noreply.github.com>
2024-06-13 19:58:42 +0900

    Add files via upload

---
+++ kadai_004/event.js
@@ -0,0 +1,8 @@
+$(window).on('load scroll', (e) => {
+  if(e.type === 'load'){ 
+    $('div').text('loadイベントが発生しました');
+   }
+  if(e.type === 'scroll'){ 
+    $('div').text('scrollイベントが発生しました');
+   }
+});
